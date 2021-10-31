d9fb2742576161e82a42e9a0c31641384cafa631
Tecnologia a Disposição de Todos <franciscoborges@msn.com>
2021-10-31 02:57:18 -0300

    Updates Diversos

--- Update.bat
+++ Update.bat
@@ -5,7 +5,13 @@ SET PATH_WZZIP="c:\Program Files\WinZip"
 TortoiseProc.exe /command:update /path:".\" /closeonend:1
 
 rem Comando para descompactar o SIACWIN.zip
-%PATH_WZZIP%\WZUnZip.exe -ybc SIACWIN.zip
+%PATH_WZZIP%\WZUnZip.exe -d -ybc SIACWIN.zip
 
 rem Comando para descompactar o CEF_BINARY.zip
 %PATH_WZZIP%\WZUnZip.exe -d -ybc CEF_BINARY.zip
+
+rem Comando para descompactar o Imagens.zip
+%PATH_WZZIP%\WZUnZip.exe -d -ybc Imagens.zip
+
+rem Comando para descompactar o Schemas.zip
+%PATH_WZZIP%\WZUnZip.exe -d -ybc Schemas.zip
