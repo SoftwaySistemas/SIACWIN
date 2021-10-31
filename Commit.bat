a11f71c60a9582684aed526f07c687944f94b6e4
Tecnologia a Disposição de Todos <franciscoborges@msn.com>
2021-10-31 01:43:05 -0300

    Update Diversos

--- Commit.bat
+++ Commit.bat
@@ -2,4 +2,4 @@
 SET SOURCE="https://github.com/SoftwaySistemas/SIACWIN.git/trunk"
 SET TARGET="*.*"
 
-start TortoiseProc.exe /command:commit /path:%Target% /logmsg:"test log message" /closeonend:1
+start TortoiseProc.exe /command:commit /path:%Target% /logmsg:"Update Diversos" /closeonend:1
