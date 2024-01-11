7617be03477d722a1cd2e6bc905f9af5812a1483
Tecnologia a Disposição de Todos <franciscoborges@msn.com>
2024-01-11 19:55:44 -0300

    Update CheckOut.bat

--- CheckOut.bat
+++ CheckOut.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SET PATH=%PATH%;..\..\TortoiseSVN\bin
-SET SOURCE="https://github.com/SoftwaySistemas/SIACWIN.git/trunk"
+SET SOURCE="https://github.com/SoftwaySistemas/SIACWIN.git"
 
 start TortoiseProc.exe /command:checkout /path:".\" /url:%Source% /blockpathadjustments /closeonend:1
