71c7da4db259fa84e101d851f1713bc0ec1ae632
Tecnologia a Disposição de Todos <franciscoborges@msn.com>
2024-01-13 18:08:22 -0300

    limpeza

--- CheckOut.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-SET PATH=%PATH%;..\..\TortoiseSVN\bin
-SET SOURCE="https://github.com/SoftwaySistemas/SIACWIN.git"
-
-start TortoiseProc.exe /command:checkout /path:".\" /url:%Source% /blockpathadjustments /closeonend:1
--- Update_COMPLETO.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET PATH_INSTALL="\\servidor\Dados\home\Instaladores\SIACWIN"
-
-if exist %Path_Install% xcopy %Path_Install%\*.* .\ /s /e /i /y /c /r
-
-del .\cef_binary.z* /q
-del .\Imagens.z* /q
-del .\Schemas.z* /q
-del .\SIACWIN.z* /q
