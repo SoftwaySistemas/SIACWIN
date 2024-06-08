6e55f9eb9301113d024aa755d9b72be08e02635e
SoftwaySistemas <franciscoborges@msn.com>
2024-06-08 11:56:00 -0300

    atualizações diversas

--- TortoiseGitProc_SIACWIN.bat
+++ TortoiseGitProc_SIACWIN.bat
@@ -2,8 +2,6 @@
 SET PATH=%PATH%;..\..\TortoiseSVN\bin
 SET PATH_WZZIP="C:\Program Files (x86)\WinZip"
 
-TortoiseGitProc.exe /command:fetch /path:".\SIACWIN.ZIP.\VAZIO.ZIP" /closeonend:2
-
 TortoiseGitProc.exe /command:pull /path:".\SIACWIN.ZIP.\VAZIO.ZIP" /closeonend:2
 
 rem Comando para descompactar o SIACWIN.zip
@@ -11,3 +9,9 @@ rem Comando para descompactar o SIACWIN.zip
 
 rem Comando para descompactar o VAZIO.zip
 %PATH_WZZIP%\WZUnZip.exe -d -ybc VAZIO.zip
+
+cd D:\server\SoftWay\SIACWIN
+start D:\server\SoftWay\SIACWIN\Launcher.exe
+
+echo.
+rem pause
